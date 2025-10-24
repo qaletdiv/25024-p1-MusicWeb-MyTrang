03ba081091bc6531d8ac2d9094fd6b1606f17fc4
JuliaTrang <hoangtrang555htm@gmail.com>
2025-10-24 17:37:18 +0700

    :sparkle: feature: adding file js for header

---
+++ code_source/html/js/header.js
@@ -0,0 +1,16 @@
+//Logo click()
+const logoImage = document.querySelector(".logo");
+logoImage.addEventListener('click',function(){
+    window.location.href = "home_page.html";
+    console.log("Homepage-logo");
+});
+//Burger click()
+const navBar = document.querySelector(".nav-bar");
+const burger = document.querySelector(".burger");
+burger.addEventListener('click', () => {
+    burger.classList.toggle('active');
+    navBar.classList.toggle('active');
+    console.log("burger click");
+});
+
+
